c53a3686a6933bd463806b6c84301247183e05d8
st162216 <146790680+st162216@users.noreply.github.com>
2023-10-24 13:27:05 +0900

    Update homework3.py

--- 2023_fall/lec03/homework3.py
+++ 2023_fall/lec03/homework3.py
@@ -19,25 +19,21 @@ def copy_all_but_skip_word(input_list, skip_word):
     If one of the elements is equal to the skip_word, then you should skip that element,
     but keep checking all of the other elements.
     '''
-    output_list = []
+    outputlist = []
 
     for x in input_list:
-        if x != skip_word:
-            output_list.append(x)
-
-    return output_list
+        if x==skip_word:
+            continue
+        outputlist.append(x)
+    return outputlist
 
 def my_average(input_list):
     '''
     You may assume that `input_list` is a non-empty list, in which every element is a number.  
     Calculate the average value, and return it. 
     '''
-    total = 0
-
+    sum = 0
     for x in input_list:
-        total += x
-
-    average = total / len(input_list)
-
-    return average
+        sum = sum + x
+    return sum / len(input_list)
 
