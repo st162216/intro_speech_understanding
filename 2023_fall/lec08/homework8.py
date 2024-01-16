e859217b9c74a8ede1d7fc7d1a5f360b1fb7bf64
st162216 <146790680+st162216@users.noreply.github.com>
2024-01-16 13:38:10 +0900

    Update homework8.py

--- 2023_fall/lec08/homework8.py
+++ 2023_fall/lec08/homework8.py
@@ -14,6 +14,6 @@ def dft_matrix(N):
     W = np.zeros((N,N), dtype='complex')
     for k in range(N):
         for n in range(N):
-            W[k,n] = np.cos(2*np.pi*k*n/N)
+            W[k,n] = np.cos(2*np.pi*k*n/N) - (0+1j)*np.sin(2*np.pi*k*n/N)
             return W
 
